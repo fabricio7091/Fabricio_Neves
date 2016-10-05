8643ea4fdbfc8ff79a39ca617f38497f19cfdd53
fabricio7091 <fabricio_sn@live.com>
2016-10-05 11:33:31 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Este script foi feito por Fabricio e Luis Tonaiser
-#Este script serve para instalar o ssh no linux e mudar porta padrão
+#Este script serve para instalar o ssh no linux e mudar a porta padrão
 
 apt-get install openssh-server -y #comando para instalação do openssh 
 sed -i "s/Port 22/Port 10000/g" /etc/ssh/sshd_config #troca da porta padrão do ssh de 22 para 1000
