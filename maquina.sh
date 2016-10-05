422b719877788c5b1f274813d10646609cf5f75e
fabricio7091 <fabricio_sn@live.com>
2016-10-05 11:35:22 -0300

    Update maquina.sh

--- maquina.sh
+++ maquina.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+#Este script foi feito por Fabricio e Luis Tonaiser
+#Este script serve para realizar uma conexão atraves do ssh
+
 ssh fabricio@10.10.65.127 cat /etc/issue ; uname -ra #comando para conexão de maquina atraves do ssh
 
 
